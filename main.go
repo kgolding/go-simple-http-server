71cd3635ca53bbb747e6db9ea4f9172a8ff12596
Kevin Golding <kevin@kgolding.co.uk>
2020-02-05 19:50:34 +0000

    add logging, use first arg as www directory, add systemd.service

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	wwwDir := flag.String("www", "www", "directory with files to be served")
+
+	log.Printf("Serving files from '%s'", *wwwDir)
+
 	http.HandleFunc("/hello", handler)
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("www"))))
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(*wwwDir))))
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
